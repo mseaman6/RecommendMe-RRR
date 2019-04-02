ca2a0f679b95cf66edb856f9cb46506b3e353839
mseaman6 <monicaameeti@gmail.com>
2019-04-02 12:45:51 -0400

    create user model and enable bcrypt

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  has_many :recommendations
+  has_many :categories, through: :recommendations
+  has_many :comments
+  has_many :commented_recommendations, :through => :comments, :source => :recommendation
+
+  validates :name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true
+
+  validates :name, uniqueness: true
+  validates :email, :uniqueness => true
+
+end
