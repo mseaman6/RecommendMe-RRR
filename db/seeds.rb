f87959c1481583636b39ad0cc6a741cff4188fd6
mseaman6 <monicaameeti@gmail.com>
2019-04-02 16:34:25 -0400

    updates to seed data removing user info

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(:name => "Monica", :email => "mls@email.com", :password => "monimoni")
-User.create(:name => "Danielle", :email => "dma@email.com", :password => "hi")
-User.create(:name => "Skittles", :email => "skittles@email.com", :password => "tastetherainbow")
-User.create(:name => "Matt", :email => "matt@email.com", :password => "poop")
-
-
 Category.create(:name => "TV")
 Category.create(:name => "MOVIE")
 Category.create(:name => "RESTAURANT")
@@ -19,13 +13,13 @@ Category.create(:name => "ACTIVITY")
 Category.create(:name => "BOOK")
 
 
-Recommendation.create(:title => "Osteria Morini", :description => "Italian (southern)", :user_id => 2, :category_id => 3)
-Recommendation.create(:title => "Acqua al 2", :description => "Italian (florentine); get the pasta and steak samplers", :user_id => 4, :category_id => 3)
-Recommendation.create(:title => "Life in Pieces", :description => "netflix show, 30 min.; funny & cute", :user_id => 1, :category_id => 1)
-Recommendation.create(:title => "Life is Beautiful", :description => "WWII flick, heartwarming and hearbreaking", :user_id => 2, :category_id => 2)
-Recommendation.create(:title => "Glass Castle", :description => "biography, growing up with weird parents", :user_id => 1, :category_id => 5)
+Recommendation.create(:title => "Osteria Morini", :description => "Italian (southern)", :category_id => 3)
+Recommendation.create(:title => "Acqua al 2", :description => "Italian (florentine); get the pasta and steak samplers", :category_id => 3)
+Recommendation.create(:title => "Life in Pieces", :description => "netflix show, 30 min.; funny & cute", :category_id => 1)
+Recommendation.create(:title => "Life is Beautiful", :description => "WWII flick, heartwarming and hearbreaking", :category_id => 2)
+Recommendation.create(:title => "Glass Castle", :description => "biography, growing up with weird parents", :category_id => 5)
 
 
-Comment.create(:text => "I don't want to read this!!!!", :user_id => 2, :recommendation_id => 5)
-Comment.create(:text => "soooo yummmy", :user_id => 1, :recommendation_id => 2)
-Comment.create(:text => "agreed", :user_id => 1, :recommendation_id => 4)
+Comment.create(:text => "I don't want to read this!!!!", :recommendation_id => 5)
+Comment.create(:text => "soooo yummmy", :recommendation_id => 2)
+Comment.create(:text => "agreed", :recommendation_id => 4)
