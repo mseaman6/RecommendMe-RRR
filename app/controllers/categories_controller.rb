ea5db98927c268f6e7fb03e32ed5f72976afa5a4
mseaman6 <monicaameeti@gmail.com>
2019-04-02 16:18:08 -0400

    create categories controller and actions

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,12 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+    render json: @categories
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    render json: @category
+  end
+end
