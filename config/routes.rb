31d481001cbdf4d314acf189a95ba4847648c934
mseaman6 <monicaameeti@gmail.com>
2019-04-02 16:45:46 -0400

    remove user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users
+    #resources :users
 
-    post 'signup', to: 'users#create', as: 'user_signup'
-    post 'login', to: 'users#login', as: 'user_login'
-    delete 'logout', to: 'users#destroy', as: 'user_logout'
+    #post 'signup', to: 'users#create', as: 'user_signup'
+    #post 'login', to: 'users#login', as: 'user_login'
+    #delete 'logout', to: 'users#destroy', as: 'user_logout'
 
     resources :recommendations do
       resources :comments
     end
-    resources :categories
 
+    resources :categories do
+      resources :recommendations do
+        resources :comments
+      end
+    end
+    
   end
+
 end
