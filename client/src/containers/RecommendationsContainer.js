1e8ad7f8e56027b60b7bdab53bafaecc2c5b5693
mseaman6 <monicaameeti@gmail.com>
2019-04-03 15:48:38 -0400

    create RecommendationsContainer set up

---
+++ client/src/containers/RecommendationsContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Recommendations from '../components/recommendations/Recommendations'
+
+import { connect } from 'react-redux'
+
+class RecommendationsContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        <Recommendations recommendations={this.props.recommendations} deleteRecommendation={this.props.deleteRecommendation} />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ recommendations }) => ({ recommendations })
+
+const mapDispatchToProps = dispatch => ({
+//  addRestaurant: text => dispatch({ type: "ADD_RESTAURANT", text }),
+  deleteRecommendation: id => dispatch({ type: "DELETE_RECOMMENDATION", id }),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecommendationsContainer)
