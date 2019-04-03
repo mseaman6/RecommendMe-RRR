a01c47d45201faa7976d9a2579d35f388b405213
mseaman6 <monicaameeti@gmail.com>
2019-04-03 14:34:37 -0400

    update to comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :text, :user_id, :commentor, :recommendation_id, :recommendation
+  attributes :id, :text, :user_id, :recommendation_id, :recommendation
 
 #  belongs_to :commentor, :class_name => "User", :foreign_key => "user_id"
   belongs_to :recommendation
