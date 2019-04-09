50aa0a3e0e9a3a76e4a3fafac46801c33d0de48a
mseaman6 <monicaameeti@gmail.com>
2019-04-09 11:05:55 -0400

    add create action to CategoriesController

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -9,4 +9,16 @@ class Api::CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     render json: @category
   end
+
+  def create
+    @category = Category.find_or_create_by(:name => params[:name].upcase)
+    if @cateogry && @category.save
+      render json: @category
+    elsif @category
+      render json: {error: "That category already exists."}
+    else
+      render json: {error: "There was a problem saving your category " + category.errors.full_messages.to_sentence}, status: :unprocessable_entity
+    end
+  end
+
 end
