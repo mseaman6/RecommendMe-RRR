b1d0fab4af9465d32dd022d7d59e0ff854bb73a1
mseaman6 <monicaameeti@gmail.com>
2019-04-09 10:51:35 -0400

    create CategoryInput component

---
+++ client/src/components/categories/CategoryInput.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { addCategory } from '../../actions/categoryActions';
+
+import { connect } from 'react-redux'
+
+class CategoryInput extends Component {
+
+  state = {
+    category: ''
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    debugger;
+    this.props.addCategory(this.state.category);
+    this.setState({
+      category: ''
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="category"
+            placeholder="or create a new category"
+            value={this.state.category}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" placeholder="Create New Category" />
+        </form>
+      </div>
+    );
+  }
+};
+
+
+export default connect(null, { addCategory })(CategoryInput)
