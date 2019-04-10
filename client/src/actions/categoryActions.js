2b351fa424dc8ab6f419d60bad0015eeb37261ac
mseaman6 <monicaameeti@gmail.com>
2019-04-10 13:36:57 -0400

    add react-bootstrap

--- client/src/actions/categoryActions.js
+++ client/src/actions/categoryActions.js
@@ -1,7 +1,7 @@
 
 export function fetchCategories() {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_CATEGORIES' });
+   return (dispatch) => {
+  //   dispatch({ type: 'LOADING_CATEGORIES' });
     return fetch(`api/categories`)
       .then(response => response.json())
       .then(categories => {
@@ -9,7 +9,7 @@ export function fetchCategories() {
         console.log(categories)
       })
       .catch(error => console.log(error))
-   };
+    };
 }
 
 // export const addCategory = category => {
