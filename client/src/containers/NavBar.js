1608a6f2aca8b09458b7ff93062030be049127db
mseaman6 <monicaameeti@gmail.com>
2019-04-12 12:35:41 -0400

    updates to NavBar

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { fetchCategories } from '../actions/categoryActions';
 
-const NavigBar = () => {
-  return (
-    <div className="container light-fade">
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">RecommendMe</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/recommendations">Recommendations</Nav.Link>
-              <Nav.Link href="/recommendations/new">Create Recommendation</Nav.Link>
-              <Nav.Link href="/categories">Categories</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
+import { connect } from 'react-redux'
+
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <div className="container light-fade">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">RecommendMe</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/recommendations">Recommendations</Nav.Link>
+                <Nav.Link href="/recommendations/new">Create Recommendation</Nav.Link>
+                <NavDropdown title="Categories" id="basic-nav-dropdown">
+
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+      </div>
+  )};
 };
 
-export default NavigBar;
+const mapStateToProps = ({ categories }) => ({ categories })
+
+export default connect(mapStateToProps, { fetchCategories })(NavigationBar)
